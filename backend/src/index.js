1b424019298bd84e1e1cce0894adf1c3446f3c05
Nosvemos <sametozturk1881@gmail.com>
2025-03-03 19:26:30 +0300

    Project folders created and required dependencies installed.

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+import { connectDb } from './lib/db.js'
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(
+  {
+    origin: process.env.CLIENT_URL,
+    credentials: true
+  }
+));
+
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectDb();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
