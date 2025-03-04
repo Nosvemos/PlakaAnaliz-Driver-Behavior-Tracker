2016709e93c4a74e4e75f2a4a9ede434c85bb5d6
Nosvemos <sametozturk1881@gmail.com>
2025-03-04 13:48:02 +0300

    Update errorHandler and errorResponse

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   if (err.code === 11000) {
-    err.message = 'Duplicate field value entered.';
+    err.message = "Duplicate field value entered.";
     err.statusCode = 400;
   }
 
@@ -9,7 +9,18 @@ const errorHandler = (err, req, res, next) => {
   const response = {
     success: false,
     message: err.message || "Internal Server Error",
+    errors: [],
   };
+
+  if (err.errors) {
+    if (typeof err.errors === "string") {
+      response.errors = [err.errors];
+    } else if (!Array.isArray(err.errors)) {
+      response.errors = [err.errors];
+    } else {
+      response.errors = err.errors.map((e) => (e.msg));
+    }
+  }
 
   res.status(statusCode).json(response);
 };
--- backend/src/utils/errorResponse.js
+++ backend/src/utils/errorResponse.js
@@ -1,9 +1,8 @@
 class errorResponse extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, errors = []) {
     super(message);
     this.statusCode = statusCode;
-    this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
+    this.errors = errors;
   }
 }
 
