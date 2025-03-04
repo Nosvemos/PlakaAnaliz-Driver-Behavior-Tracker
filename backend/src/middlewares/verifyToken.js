3118699c2e8535543629d15d9a9239a483c30ba2
Nosvemos <sametozturk1881@gmail.com>
2025-03-04 13:29:58 +0300

    Create verifyToken.js

---
+++ backend/src/middlewares/verifyToken.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+import errorResponse from "../utils/errorResponse.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return next(new errorResponse('Unauthorized - No token provided!', 401));
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if(!decoded) return next(new errorResponse('Invalid token!', 403));
+
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
