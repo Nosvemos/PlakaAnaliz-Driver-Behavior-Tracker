1c42f8625388caff4437fd80cf4a522b0ec76748
Nosvemos <sametozturk1881@gmail.com>
2025-03-05 20:50:00 +0300

    Create: notFoundPage

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,8 +1,23 @@
+import { CircleAlert } from 'lucide-react'
+import { Link } from "react-router-dom";
 import Layout from '../components/Layout.jsx'
 
 const NotFoundPage = () => {
   return (
-    <Layout></Layout>
+    <Layout>
+      <div className="flex flex-col items-center justify-center gap-4 min-h-[calc(80vh-10rem)]">
+        <CircleAlert className="size-14 text-error animate-bounce" />
+        <h1 className="font-semibold text-3xl text-center">
+          Page Not Found
+        </h1>
+        <Link
+          to="/"
+          className="btn btn-primary mt-4"
+        >
+          Go Home
+        </Link>
+      </div>
+    </Layout>
   )
 }
 
